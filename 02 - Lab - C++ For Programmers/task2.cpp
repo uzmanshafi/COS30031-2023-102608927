416e90788b243834b8c5fb467f1e9e5ec5ff60bb
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-08-07 16:28:37 +1000

    created a alter function

--- 02 - Lab - C++ For Programmers/task2.cpp
+++ 02 - Lab - C++ For Programmers/task2.cpp
@@ -8,8 +8,22 @@ void PrintTwoValues(int a, int b)
     cout << "Int 1 : " << a  << " " << "Int 2 : " << b << endl;
 }
 
+// 2. created a function that alters a value and returns them.
+int AlterValue(int a)
+{
+    a *= 4;
+    return a;
+}
+
 int main()
 {
+    //1.
     PrintTwoValues(10, 10);
+
+    //2.
+    int a = 4;
+    int alteredValue = AlterValue(a);
+    cout << "Altered Value : " << alteredValue << endl;
+
     return 0;
 }
