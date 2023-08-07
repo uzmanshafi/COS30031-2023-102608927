40bfca0e9a05626810855e84d56aff1a544c8096
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-08-07 15:16:40 +1000

    created funtion to print 2 values.

---
+++ 02 - Lab - C++ For Programmers/task2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// 1. created a function that takes two values and prints them. 
+void PrintTwoValues(int a, int b)
+{
+    cout << a << " " << b << endl;
+}
+
+int main()
+{
+    PrintTwoValues(10, 10);
+    return 0;
+}
