79fe36b488acad1a6b4376a55716a8b3df4bc8a7
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-08-07 17:26:20 +1000

    created a simple class

--- 02 - Lab - C++ For Programmers/task2.cpp
+++ 02 - Lab - C++ For Programmers/task2.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-// 1. created a function that takes two values and prints them. 
+// 1. created a function that takes two values and prints them.
 void PrintTwoValues(int a, int b)
 {
-    cout << "Int 1 : " << a  << " " << "Int 2 : " << b << endl;
+    cout << "Int 1 : " << a << " "
+         << "Int 2 : " << b << endl;
 }
 
 // 2. created a function that alters a value and returns them.
@@ -15,7 +16,7 @@ int AlterValue(int a)
     return a;
 }
 
-//3. declare a pointer to a variable and demonstrate the setting and reading of the value in the variable using the pointer
+// 3. declare a pointer to a variable and demonstrate the setting and reading of the value in the variable using the pointer
 void demoPointer()
 {
     int a = 3;
@@ -24,7 +25,7 @@ void demoPointer()
     cout << "Value of a using pointer : " << *ptr << endl;
 }
 
-//4. Create a function that uses a for loop to print all odd numbers between 0 and 20 on a single text line of text output, formatted with spaces. Call this from main.
+// 4. Create a function that uses a for loop to print all odd numbers between 0 and 20 on a single text line of text output, formatted with spaces. Call this from main.
 void PrintOddNumbers()
 {
     cout << "Odd Numbers : ";
@@ -38,7 +39,7 @@ void PrintOddNumbers()
     cout << endl;
 }
 
-//5. created a function that creates a 1 dimensional array of 5 integers, then fills the array with random int values, prints the contents of the arrat.
+// 5. created a function that creates a 1 dimensional array of 5 integers, then fills the array with random int values, prints the contents of the arrat.
 void PrintArray()
 {
     cout << "Array : ";
@@ -51,7 +52,7 @@ void PrintArray()
     cout << endl;
 }
 
-//6. created a function that splits the line of text such as "this has spaces in it"
+// 6. created a function that splits the line of text such as "this has spaces in it"
 void PrintSplitTextbySpaces(string text)
 {
     cout << "Split text: ";
@@ -64,31 +65,67 @@ void PrintSplitTextbySpaces(string text)
         endpos = text.find(" ", startpos);
     }
     cout << text.substr(startpos) << endl;
-    
 }
 
+// 7. declare a simple class that has least one private variable, public variable and public method. create an instance of the class and demo its use
+class demoClass
+{
+private:
+    int privateVar;
+
+public:
+    int publicVar;
+
+    demoClass()
+    {
+        privateVar = 0;
+        publicVar = 0;
+    }
+
+    void setPrivateVar(int value)
+    {
+        privateVar = value;
+    }
+
+    int getPrivateVar()
+    {
+        return privateVar;
+    }
+
+    void printValues()
+    {
+        cout << "Private Var : " << privateVar << endl;
+        cout << "Public Var : " << publicVar << endl;
+    }
+};
 
 int main()
 {
-    //1.
+    // 1.
     PrintTwoValues(10, 10);
 
-    //2.
+    // 2.
     int a = 4;
     int alteredValue = AlterValue(a);
     cout << "Altered Value : " << alteredValue << endl;
 
-    //3. 
+    // 3.
     demoPointer();
 
-    //4.
+    // 4.
     PrintOddNumbers();
 
-    //5.
+    // 5.
     PrintArray();
 
-    //6.
+    // 6.
     PrintSplitTextbySpaces("this has spaces in it");
+
+    // 7.
+    demoClass demo;
+    demo.setPrivateVar(10);
+    demo.publicVar = 20;
+    demo.printValues();
 
     return 0;
 }
