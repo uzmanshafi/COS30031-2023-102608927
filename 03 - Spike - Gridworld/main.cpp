86bd5e26182a57c36b6b4b6eb43890efa6526be1
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-08-09 16:14:01 +1000

    implemented game loop. switch case and update pos

--- 03 - Spike - Gridworld/main.cpp
+++ 03 - Spike - Gridworld/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 using namespace std;
 
 const char Grid_Size = 8;
@@ -11,19 +10,25 @@ const char START = 'S';
 const char GOLD = 'G';
 const char DEATH = 'D';
 
-//structure to be used to hold plauers position
+// structure to be used to hold plauers position
 struct PlayerPostion
 {
     char x;
     char y;
 };
 
-void displayGrid(const char grid[Grid_Size][Grid_Size], const PlayerPostion& playerPosition) {
-    for (int x = 0; x < Grid_Size; ++x) {
-        for (int y = 0; y < Grid_Size; ++y) {
-            if (x == playerPosition.x && y == playerPosition.y) {
+void displayGrid(const char grid[Grid_Size][Grid_Size], const PlayerPostion &playerPosition)
+{
+    for (int x = 0; x < Grid_Size; ++x)
+    {
+        for (int y = 0; y < Grid_Size; ++y)
+        {
+            if (x == playerPosition.x && y == playerPosition.y)
+            {
                 cout << 'P'; // Display the player's position
-            } else {
+            }
+            else
+            {
                 cout << grid[x][y];
             }
         }
@@ -37,15 +42,16 @@ void displayWelcomeMessage()
     cout << "Valid commands: N, S, E and W for direction. Q to quit the game." << endl;
 }
 
-
 int main()
 {
     displayWelcomeMessage();
 
     bool isGameOver = false;
     bool isPlayerWon = false;
+
     char user_cmd;
-    //create grid
+
+    // create grid
     char grid[Grid_Size][Grid_Size] = {
         {'#', 'G', ' ', 'D', '#', 'D', ' ', ' '},
         {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -54,8 +60,53 @@ int main()
         {'#', ' ', '#', '#', '#', '#', ' ', ' '},
         {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'#', '#', 'S', '#', '#', '#', '#', '#'}
-    };
+        {'#', '#', 'S', '#', '#', '#', '#', '#'}};
 
     PlayerPostion playerPosition = {8, 2};
+
+    while (!isGameOver)
+    {
+        displayGrid(grid, playerPosition);
+
+        cout << "Enter Your Command: ";
+        cin >> user_cmd;
+
+        user_cmd = toupper(user_cmd);
+
+        switch (user_cmd)
+        {
+        case 'N':
+            if (playerPosition.x > 0 && grid[playerPosition.x - 1][playerPosition.y] != WALL)
+            {
+                --playerPosition.x;
+            }
+            cout << "You have chosen to go North" << endl;
+            break;
+        case 'S':
+            if (playerPosition.x < Grid_Size - 1 && grid[playerPosition.x + 1][playerPosition.y] != WALL)
+            {
+                ++playerPosition.x;
+            }
+            cout << "You have chosen to go South" << endl;
+            break;
+        case 'E':
+            if (playerPosition.y < Grid_Size - 1 && grid[playerPosition.x][playerPosition.y + 1] != WALL)
+            {
+                ++playerPosition.y;
+            }
+            cout << "You have chosen to go East" << endl;
+            break;
+        case 'W':
+            if (playerPosition.y > 0 && grid[playerPosition.x][playerPosition.y - 1] != WALL)
+            {
+                --playerPosition.y;
+            }
+            cout << "You have chosen to go West" << endl;
+            break;
+        case 'Q':
+            cout << "You have chosen to quit the game" << endl;
+            isGameOver = true;
+            break;
+        }
+    }
 }
