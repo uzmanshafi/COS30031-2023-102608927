5847597732651c6ce98c1a32f06c9f6f337779a0
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-08-09 15:53:16 +1000

    implemented a function to display welcome message

---
+++ 03 - Spike - Gridworld/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+void displayWelcomeMessage()
+{
+    cout << "Welcome to the Gridworld: Quantised Excitement. Fate is waiting for You!" << endl;
+    cout << "Valid commands: N, S, E and W for direction. Q to quit the game." << endl;
+}
+
+
+
+
+int main()
+{
+    displayWelcomeMessage();
+}
