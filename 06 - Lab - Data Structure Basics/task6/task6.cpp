d320b3060c909bd2f345c412f6e5efc38af6edd4
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-04 03:03:03 +1100

    report question 1 completed

--- 06 - Lab - Data Structure Basics/task6/task6.cpp
+++ 06 - Lab - Data Structure Basics/task6/task6.cpp
@@ -101,7 +101,7 @@ void showIntArray(const array<int, 3>& arr);
 int main() {
     // #TODO: Uncomment each of the _demo function to investigate!!
 
-    array_demo_1();
+    //array_demo_1();
     // #TODO: Q.1 There are inside array_demo_1 - answer them there.
 
     array_demo_2();
