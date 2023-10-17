3eee5179acdbad0991bcbfcf122ef4d3605e25f1
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-18 05:08:33 +1100

    -	Created a reusable print function in main for data.

--- 10 - Lab - File Input Output/File Input Output Task 10/File Input Output Task 10/File Input Output Task 10.cpp
+++ 10 - Lab - File Input Output/File Input Output Task 10/File Input Output Task 10/File Input Output Task 10.cpp
@@ -6,5 +6,14 @@ int main() {
     myData.character = 'A';
     myData.integer = 123;
     myData.floatingPoint = 123.45;
+
+    void printData(const Data & data) {
+        cout << "Char: " << data.character << endl;
+        cout << "Int: " << data.integer << endl;
+        cout << "Float: " << data.floatingPoint << endl;
+    }
+
+    printData(myData);
+
 }
 
