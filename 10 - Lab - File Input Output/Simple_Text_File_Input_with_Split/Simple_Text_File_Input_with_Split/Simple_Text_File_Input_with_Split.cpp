5313a75274856f3884da4b639978fd704ad9ff8e
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-18 05:48:04 +1100

    modified code to ignore black lines and ignore lines with #

--- 10 - Lab - File Input Output/Simple_Text_File_Input_with_Split/Simple_Text_File_Input_with_Split/Simple_Text_File_Input_with_Split.cpp
+++ 10 - Lab - File Input Output/Simple_Text_File_Input_with_Split/Simple_Text_File_Input_with_Split/Simple_Text_File_Input_with_Split.cpp
@@ -4,6 +4,16 @@
 #include <sstream>
 #include <vector>
 
+std::vector<std::string> split(const std::string& s, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
 int main() {
     std::ifstream file("test2.txt");
     if (!file.is_open()) {
@@ -13,7 +23,21 @@ int main() {
 
     std::string line;
     while (getline(file, line)) {
-        std::cout << line << std::endl;
+        // Removes whitespace from the start and end of the string
+        line.erase(0, line.find_first_not_of(' '));
+        line.erase(line.find_last_not_of(' ') + 1);
+
+        // Ignores blank lines and lines starting with #
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        // Splits the line and print each part
+        std::vector<std::string> parts = split(line, ':');
+        for (const std::string& part : parts) {
+            std::cout << part << " ";
+        }
+        std::cout << std::endl;
     }
 
     file.close();
