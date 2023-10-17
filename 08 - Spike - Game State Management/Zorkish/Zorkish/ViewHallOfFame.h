90c9929f276e9fe39e61f1c4ee815e32f25ee39b
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-18 01:12:47 +1100

    deleted task 8 due to not meeting the requirements.

--- 08 - Spike - Game State Management/Zorkish/Zorkish/ViewHallOfFame.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "State.h"
-
-class ViewHallOfFame : public State {
-public:
-    void enter() override;
-    void handleInput(int input) override;
-    void exit() override;
-};
