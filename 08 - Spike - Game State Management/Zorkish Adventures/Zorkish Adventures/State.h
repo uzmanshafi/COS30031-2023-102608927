24622e1950d50f43efcf24358d3a3f5176ef7667
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-18 01:36:43 +1100

    creation of state base class

---
+++ 08 - Spike - Game State Management/Zorkish Adventures/Zorkish Adventures/State.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class State {
+public:
+    virtual ~State() = default;
+
+    virtual void enter() = 0;
+    virtual void exit() = 0;
+    virtual void handleInput(int input) = 0;
+};
