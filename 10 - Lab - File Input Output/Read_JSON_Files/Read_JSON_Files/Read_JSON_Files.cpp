fe644b694724bb9837ce537ce80c3c422b2378a7
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-18 06:15:59 +1100

    added json lib and printed result

--- 10 - Lab - File Input Output/Read_JSON_Files/Read_JSON_Files/Read_JSON_Files.cpp
+++ 10 - Lab - File Input Output/Read_JSON_Files/Read_JSON_Files/Read_JSON_Files.cpp
@@ -1,20 +1,28 @@
-// Read_JSON_Files.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "json.hpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using json = nlohmann::json;
+using namespace std;
+
+int main() {
+    // Opens the file
+    std::ifstream file("test3.json");
+    if (!file.is_open()) {
+        std::cerr << "Failed to open the file!" << std::endl;
+        return 1;
+    }
+
+    // Parses the file using nlohmann::json library
+    nlohmann::json jsonObj;
+    file >> jsonObj;
+
+    // Prints the content to the screen
+    for (auto& [key, value] : jsonObj.items()) {
+        std::cout << key << " : " << value.dump() << std::endl;
+    }
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
