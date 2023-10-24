f3b366ea7ef6f7c3520030af44308ae4f0192560
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-24 21:40:20 +1100

    fixed code errors in GameWorld

--- 12 - Spike - Command Pattern/Zorkish/Zorkish/GameWorldLoader.cpp
+++ 12 - Spike - Command Pattern/Zorkish/Zorkish/GameWorldLoader.cpp
@@ -1,25 +1,28 @@
 #include "GameWorldLoader.h"
 #include <fstream>
 #include "Location.h"
+#include "json.hpp" 
+
+using json = nlohmann::json;
 
 json GameWorldLoader::LoadAdventure(const std::string& filepath) {
     std::ifstream file(filepath);
     json j;
     file >> j;
     return j;
+}
 
-    std::vector<Location> GameWorldLoader::LoadLocations(const std::string & filepath) {
-        json j = LoadAdventure(filepath);
-        std::vector<Location> locations;
+std::vector<Location> GameWorldLoader::LoadLocations(const std::string& filepath) {
+    json j = LoadAdventure(filepath);
+    std::vector<Location> locations;
 
-        for (auto& locationData : j["locations"]) {
-            Location location(locationData["name"], locationData["description"]);
-            for (auto& entity : locationData["entities"]) {
-                location.addEntity(entity);
-            }
-            locations.push_back(location);
+    for (auto& locationData : j["locations"]) {
+        Location location(locationData["name"], locationData["description"]);
+        for (auto& entity : locationData["entities"]) {
+            location.addEntity(entity);
         }
-
-        return locations;
+        locations.push_back(location);
     }
+
+    return locations;
 }
--- 12 - Spike - Command Pattern/Zorkish/Zorkish/GameWorldLoader.h
+++ 12 - Spike - Command Pattern/Zorkish/Zorkish/GameWorldLoader.h
@@ -2,13 +2,14 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include "Location.h" 
 #include "json.hpp"
 
 using json = nlohmann::json;
 
-class GameWorldLoader 
+class GameWorldLoader
 {
 public:
     static json LoadAdventure(const std::string& filepath);
+    static std::vector<Location> LoadLocations(const std::string& filepath);
 };
-
