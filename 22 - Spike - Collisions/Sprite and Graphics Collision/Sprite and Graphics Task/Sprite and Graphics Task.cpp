d322a4bb7285438e3e57907abc5b0dede0d4ccff
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-25 07:10:43 +1100

    task 22 using task 17 extending code

---
+++ 22 - Spike - Collisions/Sprite and Graphics Collision/Sprite and Graphics Task/Sprite and Graphics Task.cpp
@@ -0,0 +1,108 @@
+#include <SDL.h>
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL Sprites and Graphics", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (!window) {
+        std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        SDL_DestroyWindow(window);
+        std::cerr << "Error creating SDL renderer: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    std::cout << "Loading background.bmp..." << std::endl;
+    SDL_Surface* bgSurface = SDL_LoadBMP("background.bmp");
+    if (!bgSurface) {
+        std::cerr << "Error loading background.bmp: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture* bgTexture = SDL_CreateTextureFromSurface(renderer, bgSurface);
+    SDL_FreeSurface(bgSurface);
+
+    std::cout << "Loading tiles.bmp..." << std::endl;
+    SDL_Surface* tilesSurface = SDL_LoadBMP("tiles.bmp");
+    if (!tilesSurface) {
+        std::cerr << "Error loading tiles.bmp: " << SDL_GetError() << std::endl;
+        SDL_DestroyTexture(bgTexture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture* tilesTexture = SDL_CreateTextureFromSurface(renderer, tilesSurface);
+    SDL_FreeSurface(tilesSurface);
+
+    SDL_Rect tileRects[3] = {
+        {0, 0, 50, 50},
+        {50, 0, 50, 50},
+        {100, 0, 50, 50},
+    };
+
+    bool displayBg = true;
+    bool displayTiles[3] = { false, false, false };
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                case SDLK_0:
+                    displayBg = !displayBg;
+                    std::cout << "Background display toggled " << (displayBg ? "ON" : "OFF") << "." << std::endl;
+                    break;
+                case SDLK_1:
+                case SDLK_2:
+                case SDLK_3:
+                    int tileIndex = event.key.keysym.sym - SDLK_1;
+                    displayTiles[tileIndex] = !displayTiles[tileIndex];
+                    std::cout << "Tile " << (tileIndex + 1) << " display toggled " << (displayTiles[tileIndex] ? "ON" : "OFF") << " at location (" << (rand() % 750) << ", " << (rand() % 550) << ")." << std::endl;
+                    break;
+                }
+            }
+        }
+
+        SDL_RenderClear(renderer);
+
+        if (displayBg) {
+            SDL_RenderCopy(renderer, bgTexture, NULL, NULL);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (displayTiles[i]) {
+                SDL_Rect destRect = { rand() % 750, rand() % 550, 50, 50 };
+                SDL_RenderCopy(renderer, tilesTexture, &tileRects[i], &destRect);
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    std::cout << "Cleaning up and shutting down..." << std::endl;
+    SDL_DestroyTexture(bgTexture);
+    SDL_DestroyTexture(tilesTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
