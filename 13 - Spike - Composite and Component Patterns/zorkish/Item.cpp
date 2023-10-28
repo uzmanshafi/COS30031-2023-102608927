317cf07d121a2d0d78c04aa07bd6b64e55d7ec74
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-10-29 01:12:41 +1100

    first attempt with component pattern

--- 13 - Spike - Composite and Component Patterns/zorkish/Item.cpp
+++ 13 - Spike - Composite and Component Patterns/zorkish/Item.cpp
@@ -1,15 +1,42 @@
 #include "Item.h"
+#include <algorithm>
+#include <iostream>
 
 Item::Item(const string& name, const string& desc) : _name(name), _desc(desc) {}
 
 Item::~Item() {}
 
-string Item::getName() const
-{
-	return _name;
+string Item::getName() const {
+    return _name;
 }
 
-string Item::getDesc() const
-{
-	return _desc;
+string Item::getDesc() const {
+    return _desc;
+}
+
+bool Item::hasAttribute(const string& attributeName) {
+    return attributes.find(attributeName) != attributes.end();
+}
+
+bool Item::getAttributeState(const string& attributeName) {
+    if (hasAttribute(attributeName)) {
+        return attributes[attributeName];
+    }
+
+    cerr << "Error: The attribute '" << attributeName << "' does not exist for item " << _name << "." << endl;
+    return false;
+}
+
+void Item::setAttributeState(const string& attributeName, bool state) {
+    if (hasAttribute(attributeName)) {
+        attributes[attributeName] = state;
+    }
+    else {
+
+        cerr << "Error: The attribute '" << attributeName << "' does not exist for item " << _name << "." << endl;
+    }
+}
+
+bool Item::canPerformAction(const string& actionName) {
+    return find(actions.begin(), actions.end(), actionName) != actions.end();
 }
--- 13 - Spike - Composite and Component Patterns/zorkish/Item.h
+++ 13 - Spike - Composite and Component Patterns/zorkish/Item.h
@@ -8,11 +8,16 @@ class Item
 private:
 	string _name;
 	string _desc;
-
+	map<string, bool> attributes;  // Attributes of the item.
+	vector<string> actions;        // Actions the item can perform.
 public:
 	Item(const string&, const string&);
 	~Item();
 
 	string getName() const;
 	string getDesc() const;
+	bool hasAttribute(const string& attributeName);
+	bool getAttributeState(const string& attributeName);
+	void setAttributeState(const string& attributeName, bool state);
+	bool canPerformAction(const string& actionName);
 };
