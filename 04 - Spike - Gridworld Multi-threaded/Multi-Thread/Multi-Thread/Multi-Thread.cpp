cf6ac3f18f58fcaeef34f313245cd0d24a992e8d
Shafi Uzman Fassy <102608927@student.swin.edu.au>
2023-11-06 15:44:42 +1100

    fix multi treading issues

--- 04 - Spike - Gridworld Multi-threaded/Multi-Thread/Multi-Thread/Multi-Thread.cpp
+++ 04 - Spike - Gridworld Multi-threaded/Multi-Thread/Multi-Thread/Multi-Thread.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
-#include <atomic>
 #include <mutex>
+#include <atomic>
+#include <chrono>
+#include <limits>
 #include <condition_variable>
 
 using namespace std;
 
-const int Grid_Size = 8;
+const char Grid_Size = 8;
 
 const char EMPTY = ' ';
 const char WALL = '#';
@@ -15,34 +16,35 @@ const char START = 'S';
 const char GOLD = 'G';
 const char DEATH = 'D';
 
-// Atomic flags to control the game state.
+atomic<bool> inputReceived(false);
 atomic<bool> isGameOver(false);
 atomic<bool> isPlayerWon(false);
-atomic<bool> inputAvailable(false);
+atomic<char> user_cmd(' ');
+atomic<bool> clearingScreen(false);
 
-// Mutex and condition variable for synchronizing access to input.
 mutex mtx;
 condition_variable cv;
 
-// structure to be used to hold player's position
+// structure to be used to hold players position
 struct PlayerPosition
 {
     char x;
     char y;
-} playerPosition;
+} playerPosition{7, 2}; // Initialized to start position
 
-char user_cmd;
-
-// function to display the grid.
+// Function to display the grid
 void displayGrid(const char grid[Grid_Size][Grid_Size], const PlayerPosition &playerPosition)
 {
+    lock_guard<mutex> lock(mtx);
+    clearingScreen = true;
+    system("cls"); // Clears the screen.
     for (int x = 0; x < Grid_Size; ++x)
     {
         for (int y = 0; y < Grid_Size; ++y)
         {
             if (x == playerPosition.x && y == playerPosition.y)
             {
-                cout << 'P'; // Displays the player's position
+                cout << 'P';
             }
             else
             {
@@ -51,137 +53,147 @@ void displayGrid(const char grid[Grid_Size][Grid_Size], const PlayerPosition &pl
         }
         cout << endl;
     }
+    clearingScreen = false;
 }
 
-// function to display the welcome message.
+// Welcome message
 void displayWelcomeMessage()
 {
-    cout << "Welcome to the Gridworld: Quantised Excitement. Fate is waiting for You!" << endl;
-    cout << "Valid commands: N, S, E, and W for direction. Q to quit the game." << endl;
+    cout << "Welcome to GridWorld! Find the gold and avoid traps!" << endl;
+    cout << "Controls: N (north), S (south), E (east), W (west), Q (quit)" << endl;
 }
 
-// function to process the player's input.
-void processInput()
+// Function to handles user input
+void inputThreadFunction()
 {
+    char input;
     while (!isGameOver)
     {
-        char input;
-        cout << "Enter command: "; // Prompt for input
-        cin >> input;              // Blocking call for input
+        while (clearingScreen)
+            this_thread::sleep_for(chrono::milliseconds(1));
+ 
+        {
+            // Lock to synchronize access to the console
+            lock_guard<mutex> lock(mtx);
+            cout << "Enter Your Command: ";
+        }
 
-        // Lock the mutex for safe access to shared data.
-        unique_lock<mutex> lock(mtx);
-        user_cmd = toupper(input);
-        inputAvailable = true; // Set the flag to true to indicate new input is ready.
-        lock.unlock();
+        cin >> input;
+        if (!cin)
+        {
+            // Handles input error
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            {
+                lock_guard<mutex> lock(mtx);
+                cout << "Invalid command. Please use N, S, E, W, or Q." << endl;
+            }
+            continue;
+        }
 
-        // Notify the game loop thread that new input is available.
-        cv.notify_one();
-
-        // Small sleep to prevent this thread from locking up the mutex before the game loop can acquire it.
-        this_thread::sleep_for(chrono::milliseconds(5));
+        input = toupper(input);
+        // Handles valid commands and notify the game loop
+        if (input == 'N' || input == 'S' || input == 'E' || input == 'W' || input == 'Q')
+        {
+            user_cmd = input;
+            inputReceived = true;
+            cv.notify_one();
+        }
+        else
+        {
+            // Handles invalid command
+            lock_guard<mutex> lock(mtx);
+            cout << "Invalid command. Please use N, S, E, W, or Q." << endl;
+        }
     }
 }
 
-void gameLoop(char grid[Grid_Size][Grid_Size], PlayerPosition &playerPosition)
+// Function to handles the game logic
+void gameLoopThreadFunction(char grid[Grid_Size][Grid_Size])
 {
     while (!isGameOver)
     {
-        // Lock the mutex and wait for input to be available.
         unique_lock<mutex> lock(mtx);
         cv.wait(lock, []
-                { return inputAvailable.load() || isGameOver.load(); });
+                { return inputReceived || isGameOver; }); // Waits for input or game over
 
-        if (isGameOver)
+        if (inputReceived)
         {
-            break;
-        }
+            // Process the input
+            switch (user_cmd)
+            {
+            case 'N':
+                if (playerPosition.x > 0 && grid[playerPosition.x - 1][playerPosition.y] != WALL)
+                {
+                    playerPosition.x--;
+                    cout << "You have chosen to go North" << endl;
+                }
+                else
+                {
+                    cout << "You cannot go that way!" << endl;
+                }
+                break;
+            case 'S':
+                if (playerPosition.x < Grid_Size - 1 && grid[playerPosition.x + 1][playerPosition.y] != WALL)
+                {
+                    playerPosition.x++;
+                    cout << "You have chosen to go South" << endl;
+                }
+                else
+                {
+                    cout << "You cannot go that way!" << endl;
+                }
+                break;
+            case 'E':
+                if (playerPosition.y < Grid_Size - 1 && grid[playerPosition.x][playerPosition.y + 1] != WALL)
+                {
+                    playerPosition.y++;
+                    cout << "You have chosen to go East" << endl;
+                }
+                else
+                {
+                    cout << "You cannot go that way!" << endl;
+                }
+                break;
+            case 'W':
+                if (playerPosition.y > 0 && grid[playerPosition.x][playerPosition.y - 1] != WALL)
+                {
+                    playerPosition.y--;
+                    cout << "You have chosen to go West" << endl;
+                }
+                else
+                {
+                    cout << "You cannot go that way!" << endl;
+                }
+                break;
+            case 'Q':
+                cout << "You have chosen to quit the game" << endl;
+                isGameOver = true;
+                break;
+            default:
+                break;
+            }
 
-        char command = user_cmd; // Copy the input command.
-        user_cmd = '\0';         // Reset the shared input command.
-        inputAvailable = false;  // Reset the input available flag.
-        lock.unlock();           // Unlock as soon as the necessary data is copied.
+            // Checks game state
+            if (grid[playerPosition.x][playerPosition.y] == GOLD)
+            {
+                cout << "Wow - you have discovered a large chest filled with GOLD coins!" << endl;
+                cout << "YOU WIN!" << endl;
+                isPlayerWon = true;
+                isGameOver = true;
+            }
+            else if (grid[playerPosition.x][playerPosition.y] == DEATH)
+            {
+                cout << "Arrrrgh... you’ve fallen down a pit." << endl;
+                cout << "YOU HAVE DIED!" << endl;
+                isPlayerWon = false;
+                isGameOver = true;
+            }
 
-        // Handles user input
-        switch (command)
-        {
-        case 'N':
-            if (playerPosition.x > 0 && grid[playerPosition.x - 1][playerPosition.y] != WALL)
-            {
-                --playerPosition.x;
-                cout << "You have chosen to go North" << endl;
-            }
-            else
-            {
-                cout << "You cannot go that way!" << endl;
-            }
-            break;
-        case 'S':
-            if (playerPosition.x < Grid_Size - 1 && grid[playerPosition.x + 1][playerPosition.y] != WALL)
-            {
-                ++playerPosition.x;
-                cout << "You have chosen to go South" << endl;
-            }
-            else
-            {
-                cout << "You cannot go that way!" << endl;
-            }
-            break;
-        case 'E':
-            if (playerPosition.y < Grid_Size - 1 && grid[playerPosition.x][playerPosition.y + 1] != WALL)
-            {
-                ++playerPosition.y;
-                cout << "You have chosen to go East" << endl;
-            }
-            else
-            {
-                cout << "You cannot go that way!" << endl;
-            }
-            break;
-        case 'W':
-            if (playerPosition.y > 0 && grid[playerPosition.x][playerPosition.y - 1] != WALL)
-            {
-                --playerPosition.y;
-                cout << "You have chosen to go West" << endl;
-            }
-            else
-            {
-                cout << "You cannot go that way!" << endl;
-            }
-            break;
-        case 'Q':
-            isGameOver = true;
-            cout << "You have chosen to quit the game." << endl;
-            break;
-        default:
-            cout << "Invalid command!" << endl;
-            break;
+            inputReceived = false;             // Resets the input flag for the next iteration
+            lock.unlock();                     // Unlocks before prompting for the next input
+            displayGrid(grid, playerPosition); // Updates the display after each move
         }
-
-        // Check the result of the move
-        if (grid[playerPosition.x][playerPosition.y] == GOLD)
-        {
-            cout << "Wow - you have discovered a large chest filled with GOLD coins!" << endl;
-            cout << "YOU WIN!" << endl;
-            isPlayerWon = true;
-            isGameOver = true;
-        }
-        else if (grid[playerPosition.x][playerPosition.y] == DEATH)
-        {
-            cout << "Arrrrgh... you’ve fallen into a pit." << endl;
-            cout << "YOU HAVE DIED!" << endl;
-            isPlayerWon = false;
-            isGameOver = true;
-        }
-
-        // If input was 'Q', no need to display the grid or prompt again.
-        if (command != 'Q')
-        {
-            displayGrid(grid, playerPosition); // Display the grid with the updated player position.
-        }
-
-        // Notifies the input thread that it can accept another input
-        cv.notify_one();
     }
 }
 
@@ -189,7 +201,7 @@ int main()
 {
     displayWelcomeMessage();
 
-    // creates grid
+    // Initialize grid
     char grid[Grid_Size][Grid_Size] = {
         {'#', '#', '#', '#', '#', '#', '#', '#'},
         {'#', 'G', ' ', 'D', '#', 'D', ' ', '#'},
@@ -200,22 +212,20 @@ int main()
         {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
         {'#', '#', 'S', '#', '#', '#', '#', '#'}};
 
-    // Sets the player's initial position
-    PlayerPosition playerPosition = {7, 2};
+    displayGrid(grid, playerPosition);
 
-    // Starts the input processing in a separate thread
-    thread inputThread(processInput);
+    // Starts threads
+    thread inputThread(inputThreadFunction);
+    thread gameThread(gameLoopThreadFunction, grid);
 
-    // Starts the game loop in the main thread
-    gameLoop(grid, playerPosition);
-
-    // Waits for the input thread to finish before exiting the program
+    // Waits for threads to finish
     inputThread.join();
+    gameThread.join();
 
-    // Final game state message
+    // Game over messages displays
     if (isPlayerWon)
     {
-        cout << "Thanks for playing. You've won!" << endl;
+        cout << "Thanks for playing. You Won!." << endl;
     }
     else
     {
